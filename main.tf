a335d09fed239b60c0968575a350f00a15e7ecbc
Ashvin Nihalani <ashvin.nihalani@gmail.com>
2023-03-18 04:49:59 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,3 +13,17 @@ terraform {
     }
   }
 }
+
+resource "linode_instance" "web" {
+    label = "simple_instance"
+    image = "linode/ubuntu18.04"
+    region = "us-central"
+    type = "g6-standard-1"
+    authorized_keys = ["ssh-rsa AAAA...Gw== user@example.local"]
+    root_pass = "terr4form-test"
+
+    group = "foo"
+    tags = [ "foo" ]
+    swap_size = 256
+    private_ip = true
+}
